417af637983353f9cd3a7c6137b3a86f71ed3ee7
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2023-06-11 02:45:48 +0530

    Initial Commit

---
+++ BreakingBadAPI/BreakingBadAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadAPIApp.swift
+//  BreakingBadAPI
+//
+//  Created by Lucas Pereira on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakingBadAPI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BreakingBadAPI
+//
+//  Created by Lucas Pereira on 11/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
