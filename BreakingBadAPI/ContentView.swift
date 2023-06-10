edd11c708f2b9ae4fe15379817e31c4d2ccc5f5c
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2023-06-11 03:26:06 +0530

    Reading API and showing in a list

--- BreakingBadAPI/ContentView.swift
+++ BreakingBadAPI/ContentView.swift
@@ -7,15 +7,47 @@
 
 import SwiftUI
 
+struct Info : Codable, Hashable {
+    var quote: String
+    var author: String
+}
+
 struct ContentView: View {
+    
+    @State private var infos = [Info]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List (infos, id: \.self) {info in
+                VStack(alignment: .leading) {
+                    Text("\"\(info.quote)\"")
+                    Text(info.author)
+                        .foregroundColor(.gray)
+                        .font(.headline)
+                }
+            }
+            .task {
+                await fetchData()
+            }
         }
         .padding()
+    }
+    
+    // create URL and fetch data
+    func fetchData() async {
+        guard let url = URL(string: "https://api.breakingbadquotes.xyz/v1/quotes/50") else {
+            print("URL does not exists")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            // decode the data
+            if let decodedResponse = try? JSONDecoder().decode([Info].self, from: data) {
+                infos = decodedResponse
+            }
+        } catch {
+            print("Invalid data or failed to fetch request")
+        }
     }
 }
 
